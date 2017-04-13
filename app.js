e931abdb0ecc15be5b9d53692386dbbeb61b7ad8
msolorio <msolorio789@gmail.com>
2017-04-12 18:03:14 -0700

    add basic flow from step to step

--- app.js
+++ app.js
@@ -2,19 +2,19 @@
 var data = {
   steps: [
     {
-      stepName: 'intro',
       stepType: 'intro',
       content: {
+        stepName: 'intro',
         headline: 'Take a Quiz on Circuits',
         button: 'Start Quiz'
       }
     },
 
     {
-      stepName: 'question1',
       stepType: 'question',
       questionNum: 1,
       content: {
+        stepName: 'question1',
         question: 'question1?',
         responseChoices: [
           'response1',
@@ -28,10 +28,10 @@ var data = {
     },
 
     {
-      stepName: 'question2',
       stepType: 'question',
       questionNum: 2,
       content: {
+        stepName: 'question2',
         question: 'question2?',
         responseChoices: [
           'response1',
@@ -45,10 +45,10 @@ var data = {
     },
 
     {
-      stepName: 'question3',
       stepType: 'question',
       questionNum: 3,
       content: {
+        stepName: 'question3',
         question: 'question3?',
         responseChoices: [
           'response1',
@@ -62,10 +62,10 @@ var data = {
     },
 
     {
-      stepName: 'question4',
       stepType: 'question',
       questionNum: 4,
       content: {
+        stepName: 'question4',
         question: 'question4?',
         responseChoices: [
           'response1',
@@ -79,10 +79,10 @@ var data = {
     },
 
     {
-      stepName: 'question5',
       stepType: 'question',
       questionNum: 5,
       content: {
+        stepName: 'question5',
         question: 'question5?',
         responseChoices: [
           'response1',
@@ -96,21 +96,67 @@ var data = {
     },
 
     {
-      stepName: 'end',
       stepType: 'end',
       content: {
+        stepName: 'end',
         headline: 'Overall Score',
         button: 'Start New Quiz'
       }
     }   
   ]
-}
+};
 
 var state = {
-  currentStep: 0,
+  currentStepNum: 0,
   chosenResponse: null,
   chosenResponseCorrect: null,
   numOfCorrect: 0,
-  numOfIncorrect: 0,
-  correctResponseShow: false
+  numOfIncorrect: 0
 }
+
+// CHECKING STATE ///////////////////////////////////////////////////////
+function checkResponseValidity(state, stepData) {
+
+}
+
+function responseChosen(state) {
+  return true;
+}
+
+// UPDATING STATE ///////////////////////////////////////////////////////
+function updateState(state, data) {
+
+  state.currentStepNum++;
+  console.log(state);
+};
+
+// RENDERING STATE ///////////////////////////////////////////////////////
+function renderState(state, data, dom) {
+  var stepContent = data.steps[state.currentStepNum].content;
+  var stepName = stepContent.stepName;
+  var button = stepContent.button;
+
+  dom.stepName.text(stepName);
+  dom.button.html(button);
+};
+
+
+// EVENT LISTENERS /////////////////////////////////////////////////////////
+function handleButtonClick(dom) {
+  dom.button.click(function(event) {
+    event.preventDefault();
+    console.log(state);
+
+    updateState(state, data);
+    renderState(state, data, dom);
+  });
+};
+
+// keep event listeners and jquery object variables in jQuery ready function
+$(function() {
+  var dom = {};
+  dom.stepName = $('.js-stepName');
+  dom.button = $('.js-button');
+
+  handleButtonClick(dom);
+});

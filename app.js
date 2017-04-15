50dd8d31e17c0c7dd7389b3027b4900a78a25a27
msolorio <msolorio789@gmail.com>
2017-04-15 14:59:24 -0700

    implement state change for questions and add rendering of choose response message

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+// TODO: add increment score card functionality on state DONE
+// TODO: on render if response chosen disable radio buttons
+
 // can get rid of stepName later - using for wiring up basic flow
 var data = {
+  numOfQuestions: 5,
   steps: [
     {
       stepType: 'intro',
@@ -22,7 +26,7 @@ var data = {
         responseChoice4: 'responseChoice4',
         button: 'Submit'
       },
-      correctResponse: 'response1'
+      correctResponse: '0'
     },
 
     {
@@ -37,7 +41,7 @@ var data = {
         responseChoice4: 'responseChoice4',
         button: 'Submit'
       },
-      correctResponse: 'response1'
+      correctResponse: '0'
     },
 
     {
@@ -52,7 +56,7 @@ var data = {
         responseChoice4: 'responseChoice4',
         button: 'Submit'
       },
-      correctResponse: 'response1'
+      correctResponse: '0'
     },
 
     {
@@ -67,7 +71,7 @@ var data = {
         responseChoice4: 'responseChoice4',
         button: 'Submit'
       },
-      correctResponse: 'response1'
+      correctResponse: '0'
     },
 
     {
@@ -82,7 +86,7 @@ var data = {
         responseChoice4: 'responseChoice4',
         button: 'Submit'
       },
-      correctResponse: 'response1'
+      correctResponse: '0'
     },
 
     {
@@ -96,75 +100,175 @@ var data = {
   ]
 };
 
-var state = {
+window.state = {
   currentStepNum: 0,
+  currentQuestionNum: 0,
   chosenResponse: null,
-  chosenResponseCorrect: null,
+  responseChosen: false,
+  showCorrectResponse: false,
+  alertToChooseResponse: false,
   numOfCorrect: 0,
   numOfIncorrect: 0
 }
 
-// CHECKING STATE ///////////////////////////////////////////////////////
-function checkResponseValidity(state, stepData) {
-
-}
-
-function responseChosen(state) {
-  return true;
-}
-
-// UPDATING STATE ///////////////////////////////////////////////////////
-function updateState(state, data) {
-
-  state.currentStepNum++;
-  
+// RETRIEVING INFORMATON FROM DOM ///////////////////////////////////////
+function getChosenResponse(state, dom) {
+  return $('input[type=radio]:checked').val();
 };
 
+// CHECKING STATE ///////////////////////////////////////////////////////
 
+// UPDATING STATE ///////////////////////////////////////////////////////
 
-// // RENDERING STATE ///////////////////////////////////////////////////////
+// if response not chosen update state to show alert to choose response
+function updateStateAlertToChooseResponse(state) {
+  if (!state.responseChosen) state.alertToChooseResponse = true;
+  else state.alertToChooseResponse = false;
+}
+
+function incrementScoreCard(state, data) {
+  var correctResponse = data.steps[state.currentStepNum].correctResponse;
+  
+  if (state.chosenResponse === correctResponse.toString()) state.numOfCorrect++;
+  else state.numOfIncorrect++;
+}
+
+function updateStateShowCorrectResponse(state, data) {
+  state.showCorrectResponse = true;
+  incrementScoreCard(state, data);
+}
+
+function updateStateNextStep(state) {
+  state.chosenResponse = null;
+  state.responseChosen = false;
+  state.showCorrectResponse = false;
+  state.currentQuestionNum++;
+  state.currentStepNum++;
+}
+
+function handleStateCorrectResponse(state, data) {
+  // if chosen response and correct response not shown
+  // update state to show correct response
+  if (state.chosenResponse && !state.showCorrectResponse) {
+    updateStateShowCorrectResponse(state, data);
+
+  // if chosen response and correct response shown
+  // update state for next step
+  } else if (state.chosenResponse && state.showCorrectResponse) {
+    updateStateNextStep(state);
+  }
+}
+
+function updateStateOnQuestion(state, data, dom) {
+  state.chosenResponse = getChosenResponse(state, dom) || null;
+  state.responseChosen = state.chosenResponse !== null ? true : false;
+  updateStateAlertToChooseResponse(state);
+  handleStateCorrectResponse(state, data);
+}
+
+function updateState(state, data, dom) {
+  var stepType = data.steps[state.currentStepNum].stepType;
+  if (stepType === 'question') {
+    updateStateOnQuestion(state, data, dom);
+  } else {
+    state.currentStepNum++;
+  }
+  console.log('state after update state:', state);
+};
+
+// RENDERING STATE ///////////////////////////////////////////////////////
+function handleLayout(state, data, dom) {
+  var stepType = data.steps[state.currentStepNum].stepType;
+  var responseChoices = $('.js-responseChoices');
+  if (stepType === 'question') responseChoices.show();
+  else responseChoices.hide();
+};
 
 // allElements is all possible dom elements
 // if content exists for that element fill it in
 // if not remove the element
 function renderStepContent(dom, stepContent) {
-  var allElements = Object.keys(dom);
+  var domContent = dom.domContent;
+  var allElements = Object.keys(domContent);
   allElements.forEach(function(domElement) {
     if (stepContent[domElement]) {
-      $(dom[domElement]).html(stepContent[domElement]).show();
+      $(domContent[domElement]).html(stepContent[domElement]).show();
     } else {
-      $(dom[domElement]).html('').hide();
+      $(domContent[domElement]).html('').hide();
     }
   });
-}
+};
+
+function renderChooseAlertResponse(state, data, dom) {
+  if (state.alertToChooseResponse) {
+    $(dom.message).html('Please Choose a Response');
+  } else {
+    $(dom.message).html('');
+  }
+};
+
+function renderShowCorrectResponse() {
+  //
+};
+
+function renderNextStep() {
+  //
+};
+
+function handleRenderCorrectResponse() {
+  
+  renderNextStep();
+
+  renderShowCorrectResponse();
+};
+
+function renderStateOnQuestion(state, data, dom) {
+    // if a response has not been chosen
+    // TODO: will be moved to proper place
+    if (!state.responseChosen) $(dom.checkedRadioButton).prop('checked', false);
+
+    renderChooseAlertResponse(state, data, dom);
+    handleRenderCorrectResponse();
+};
 
 function renderState(state, data, dom) {
-  var stepContent = data.steps[state.currentStepNum].content;
+  console.log('in renderState');
+  var step = data.steps[state.currentStepNum];
+  var stepContent = step.content;
+  if (step.stepType === 'question') {
+    renderStateOnQuestion(state, data, dom);
+  }
   renderStepContent(dom, stepContent);
 };
 
 
 // EVENT LISTENERS /////////////////////////////////////////////////////////
 function handleButtonClick(dom) {
-  dom.button.click(function(event) {
+  $(dom.domContent.button).click(function(event) {
     event.preventDefault();
-    console.log(state);
+    console.log('state after button click:', state);
 
-    updateState(state, data);
+    updateState(state, data, dom);
+    handleLayout(state, data, dom);
     renderState(state, data, dom);
   });
 };
 
 // keep event listeners and jquery object variables in jQuery ready function
 $(function() {
-  var dom = {};
-  dom.stepName = $('.js-stepName');
-  dom.headline = $('.js-headline');
-  dom.button = $('.js-button');
-  dom.question = $('.js-question');
-  dom.responseChoice1 = $('.js-responseChoice1');
-  dom.responseChoice2 = $('.js-responseChoice2');
-  dom.responseChoice3 = $('.js-responseChoice3');
-  dom.responseChoice4 = $('.js-responseChoice4');
+  var dom = {
+    domContent: {}
+  };
+  dom.checkedRadioButton = 'input[type=radio]:checked';
+  dom.message = '.js-message';
+
+  dom.domContent.stepName = '.js-stepName';
+  dom.domContent.headline = '.js-headline';
+  dom.domContent.button = '.js-button';
+  dom.domContent.question = '.js-question';
+  dom.domContent.responseChoice1 = '.js-responseChoice1';
+  dom.domContent.responseChoice2 = '.js-responseChoice2';
+  dom.domContent.responseChoice3 = '.js-responseChoice3';
+  dom.domContent.responseChoice4 = '.js-responseChoice4';
   handleButtonClick(dom);
 });
